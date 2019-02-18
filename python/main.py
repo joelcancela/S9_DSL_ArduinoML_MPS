2ce37a96e39e736c40292919d214f19cc11e788a
Nikita Rousseau <nikita.rousseau@etu.unice.fr>
2019-02-18 22:05:27 +0100

    wip python server (parse mode and state)

--- python/main.py
+++ python/main.py
@@ -2,6 +2,43 @@
 import serial.tools.list_ports
 import serial
 
+
+def serial_to_key_value(raw_string):
+    ret = {}
+    frags = str(raw_string.split(':')[1]).split(',')
+    for frag in frags:
+        pair = frag.split('=')
+        ret[pair[0]] = pair[1]
+    return ret
+
+
+def serial_to_mode_state(raw_string):
+    ret = {}
+    frags = raw_string.split(',')
+    ret['mode'] = str(frags[0])
+    ret['state'] = str(frags[1])
+    return ret
+
+
+def bind_mode_state_to_human(dict_ctx, dict_modes, dict_states):
+    h_mode = "Unknown mode"
+    h_state = "Uknown state"
+
+    # resolve mode
+    for key, value in dict_modes.items():
+        if value == dict_ctx['mode']:
+            h_mode = key
+            break
+
+    # resolve state
+    for key, value in dict_states.items():
+        if value == dict_ctx['state']:
+            h_state = key
+            break
+
+    return "[" + h_mode + "] " + h_state + " -- "
+
+
 port = ""
 ports = list(serial.tools.list_ports.comports())
 try:
@@ -14,21 +51,36 @@ print("Connected on " + str(port) + "...")
 
 print("Waiting for communication...")
 
-with serial.Serial(port.device, 19200, timeout=5) as ser:
+with serial.Serial(port.device, 19200, timeout=10) as ser:
     try:
         probe = ser.read().decode("utf-8").strip()
         if probe == '*':
             # sync
             print('Ready !')
-            hello = str(ser.readline().decode("utf-8").strip())     # #hello:
-            states = str(ser.readline().decode("utf-8").strip())    # #states:
-            modes = str(ser.readline().decode("utf-8").strip())     # #modes:
-            params = str(ser.readline().decode("utf-8").strip())    # #params:
-            eoi = str(ser.readline().decode("utf-8").strip())       # #eoi:
-            monitor = str(ser.readline().decode("utf-8").strip())   # #monitor:
+
+            # parse hello initialization header
+            print('Initialization...')
+
+            hello = str(ser.readline().decode("utf-8").strip())         # #hello:       -- SKIP
+
+            raw_states = str(ser.readline().decode("utf-8").strip())    # #states:
+            states = serial_to_key_value(raw_states)
+
+            raw_modes = str(ser.readline().decode("utf-8").strip())     # #modes:
+            modes = serial_to_key_value(raw_modes)
+
+            raw_params = str(ser.readline().decode("utf-8").strip())    # #params:
+            params = serial_to_key_value(raw_params)
+
+            eoi = str(ser.readline().decode("utf-8").strip())           # #eoi:         -- SKIP
+            monitor = str(ser.readline().decode("utf-8").strip())       # #monitor:     -- SKIP
+
+            print('End of initialization...')
+
             while True:
-                raw_input = str(ser.readline().decode("utf-8").strip())
-                print(raw_input)
+                ctx = serial_to_mode_state(str(ser.readline().decode("utf-8").strip()))
+                h = bind_mode_state_to_human(ctx, modes, states)
+                print(h)
         else:
             # no sync
             print('Unable to synchronize. Abort !')
